581d3e1be6433355626fd3704921899e911c0206
Olaoluwa22 <olaoluwaoni22@gmail.com>
2024-04-15 17:45:15 +0100

    Configured the JwtAuthenticationFilter

--- src/main/java/com/GlamourByNora/api/config/WebSecurityConfig.java
+++ src/main/java/com/GlamourByNora/api/config/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.GlamourByNora.api.config;
 
+import com.GlamourByNora.api.jwt.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,9 +13,13 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
@@ -21,6 +27,8 @@ public class WebSecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(AbstractHttpConfigurer::disable)
                 .sessionManagement(configure -> configure.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        httpSecurity
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         httpSecurity
                 .authorizeHttpRequests(authorize->authorize
                         .requestMatchers("/auth/signup", "/auth/login").permitAll()
--- src/main/java/com/GlamourByNora/api/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/GlamourByNora/api/jwt/JwtAuthenticationFilter.java
@@ -7,13 +7,14 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.apache.coyote.BadRequestException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-
-public class JwtTokenFilter extends OncePerRequestFilter {
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private JwtService jwtService;
-    public JwtTokenFilter(JwtService jwtService) {
+    public JwtAuthenticationFilter(JwtService jwtService) {
         this.jwtService = jwtService;
     }
 
